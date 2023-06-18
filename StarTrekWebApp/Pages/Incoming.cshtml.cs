a26e3e372a358b44e54f29659620380abba5a95a
andregoncrod <andrerodrigues.pombal@gmail.com>
2023-06-18 18:55:22 +0100

    First version without spacecrafts

---
+++ StarTrekWebApp/Pages/Incoming.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarTrekWebApp.Pages
+{
+    public class IncomingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ StarTrekWebApp/Pages/Spacecrafts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarTrekWebApp.Pages
+{
+    public class SpacecraftsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
